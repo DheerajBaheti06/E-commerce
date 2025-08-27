2e09537de47ad3b256e2783f02540a263655f3ca
DheerajBaheti06 <dheeraj.baheti.2023@gmail.com>
2025-08-28 00:35:36 +0530

    feta(backend): add file uploading utils and middleware.

---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv"
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+dotenv.config({
+    path: "../config/config.env"
+  })
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded successfull
+        fs.unlinkSync(localFilePath)
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        console.log("error in cloudinary", error);
+        return null;
+    }
+}
+
+
+
+export {uploadOnCloudinary}
